13b06e68ba52b4f58cf72ab5d8ca76b842d95797
Psaikishanrao <saikishan785@gmail.com>
2024-08-09 10:22:55 +0530

    trying to deploy with netlify

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: npm install && npm run build && npm start
-json: json-server --watch db.json --port 3030
---
+++ netlify/functions/jsonServer.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const serverless = require('serverless-http');
+
+const app = express();
+
+
+const dbFilePath = path.resolve(__dirname, '../../db.json'); 
+const dbData = JSON.parse(fs.readFileSync(dbFilePath, 'utf-8'));
+
+app.get('/api/:resource', (req, res) => {
+  const resource = req.params.resource;
+  const data = dbData[resource];
+
+  if (!data) {
+    return res.status(404).json({ error: 'Resource not found' });
+  }
+
+  res.json(data);
+});
+
+app.get('*', (req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
+module.exports.handler = serverless(app);
