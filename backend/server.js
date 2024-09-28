013550e37a0085f9ea9b5171b7e22dd62f015cff
Psaikishanrao <saikishan785@gmail.com>
2024-09-29 00:44:35 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://disel-git-main-psaikishanraos-projects.vercel.app", 
+  methods: ["GET", "POST", "PUT", "DELETE"], 
+  credentials: false, 
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI, {
